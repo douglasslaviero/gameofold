afd052222499a3a01b4bbbe0051203e6e222d1e1
douglasslaviero <emaildodouglasslaviero@gmail.com>
2021-04-18 18:11:14 -0300

    Adjusted to set image to players buttons

--- app/src/main/java/br/ucs/android/gameofold/MainActivity.java
+++ app/src/main/java/br/ucs/android/gameofold/MainActivity.java
@@ -1,10 +1,8 @@
 package br.ucs.android.gameofold;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
+import android.Manifest;
 import android.content.Intent;
-import android.database.Cursor;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -12,7 +10,12 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,79 +24,103 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int GALERIA_IMAGENS = 1;
+    private final int PERMISSAO_REQUEST = 2;
     private final int SELFIE1 = 3;
     private final int SELFIE2 = 4;
 
     private File selfieFile1 = null;
     private File selfieFile2 = null;
 
-    private ImageView selfie1;
-    private ImageView selfie2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Pede permissão para acessar as mídias gravadas no dispositivo
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PERMISSAO_REQUEST);
+            }
+        }
+
+        // Pede permissão para escrever arquivos no dispositivo
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSAO_REQUEST);
+            }
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK && requestCode == SELFIE1){
+        if (resultCode == RESULT_OK && requestCode == SELFIE1) {
             sendBroadcast(new Intent(
                     Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
                     Uri.fromFile(selfieFile1)
             ));
-            showSelfie1(selfieFile1.getAbsolutePath());
+            showSelfie(selfieFile1.getAbsolutePath(), R.id.player1ImgButton);
         }
 
-        if(resultCode == RESULT_OK && requestCode == SELFIE2){
+        if (resultCode == RESULT_OK && requestCode == SELFIE2) {
             sendBroadcast(new Intent(
                     Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
                     Uri.fromFile(selfieFile2)
             ));
-            showSelfie1(selfieFile2.getAbsolutePath());
+            showSelfie(selfieFile2.getAbsolutePath(), R.id.player2ImgButton);
         }
     }
 
-    private void showSelfie1(String path){
+    private void showSelfie(String path, int viewId) {
         Bitmap bitmap = BitmapFactory.decodeFile(path);
-        selfie1.setImageBitmap(bitmap);
+        ImageButton ib = findViewById(viewId);
+        ib.setImageBitmap(bitmap);
     }
 
-    private void showSelfie2(String path){
-        Bitmap bitmap = BitmapFactory.decodeFile(path);
-        selfie1.setImageBitmap(bitmap);
+    public void clear(View view) {
+        ImageButton ib1 = findViewById(R.id.player1ImgButton);
+        ib1.setImageBitmap(null);
+        ImageButton ib2 = findViewById(R.id.player2ImgButton);
+        ib2.setImageBitmap(null);
     }
 
-    public void takeASelfie1(View view)
-    {
+    public void takeASelfie1(View view) {
         this.takeASelfie(view, selfieFile1, SELFIE1);
     }
 
-    public void takeASelfie2(View view)
-    {
+    public void takeASelfie2(View view) {
         this.takeASelfie(view, selfieFile2, SELFIE2);
     }
 
-    public void takeASelfie(View view, File selfieFile, int selfieCode)
-    {
+    public void takeASelfie(View view, File selfieFile, int selfieCode) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
-        {
-            try
-            {
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            try {
                 selfieFile = createFile();
-            }
-            catch (IOException ex)
-            {
+            } catch (IOException ex) {
                 //mostraAlerta(getString(R.string.erro), getString(R.string.erro_salvando_foto));
             }
 
-            if (selfieFile != null)
-            {
+            if (selfieCode == SELFIE1)
+                selfieFile1 = selfieFile;
+            else
+                selfieFile2 = selfieFile;
+
+            if (selfieFile != null) {
                 Uri photoURI = FileProvider.getUriForFile(
                         getBaseContext(),
                         getBaseContext().getApplicationContext().getPackageName() + ".provider",
@@ -105,8 +132,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private File createFile() throws IOException
-    {
+    private File createFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_Hhmmss").format(new Date());
         File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File image = new File(folder.getPath() + File.separator + "JPG_" + timeStamp + ".jpg");
