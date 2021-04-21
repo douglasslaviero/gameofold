d33db19f8e14249d38830829b27d4631d2a5ad56
yagobrandalise <brandaliseyago.gmail.com>
2021-04-21 18:33:03 -0300

    Added draw check, image button content adjusted and winner now is highlighed

---
+++ app/src/main/java/br/ucs/android/gameofold/model/WinCondition.java
@@ -0,0 +1,12 @@
+package br.ucs.android.gameofold.model;
+
+public enum WinCondition {
+    FirstLine,
+    SecondLine,
+    ThirdLine,
+    FirstColumn,
+    SecondColumn,
+    ThirdColumn,
+    CrescDiagonal,
+    DecrescDiagonal
+}
